000ae8994ec8ca695602ccb263ecf1e8fc8ee4c5
sameer-bhujel <bhujelsameer143@gmail.com>
2022-07-14 12:51:02 +0545

    project init

--- it_bridge_management/settings.py
+++ it_bridge_management/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config,Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2bj*64f*l__s#rqtc2tg1ak&#v6h+5b9x9d2=-lnan@n__7hrf'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = config('DEBUG', default=True, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOST', default='127.0.01', cast=Csv())
 
 
 # Application definition
