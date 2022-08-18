87bb149e9611efdafb7d6fbbb56ee4a5fa535a91
Binod Kafle <mebinod50@gmail.com>
2022-08-18 07:42:46 +0545

    chore: add phone number validation

--- itbridge/serializers/user.py
+++ itbridge/serializers/user.py
@@ -12,7 +12,12 @@ class UserSerializer(serializers.ModelSerializer):
         if image.size > 1 * 1024 * 1024:
             raise serializers.ValidationError("Image must be less than 1 MB.")
         return image
-    def validation_phone_number(self,phone_number):
-        if phone_number.size > 4+10:
-            raise serializers.ValidationError("Phonenumber must be equal ko 14 digits")
+
+    def validate_phone_number(self, phone_number):
+        if len(phone_number) > 10:
+            raise serializers.ValidationError("Phone number cannot be more than 10 digits.")
+        if len(phone_number) < 10:
+            raise serializers.ValidationError("Phone number cannot be less than 10 digits.")
+        if phone_number[:2] != "98" and phone_number[:2] != "97":
+            raise serializers.ValidationError("Phone number should start with 98 or 97.")
         return phone_number
