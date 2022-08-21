cc2bace0d19569ea3af49e9363026aca5c26fc4f
Binod Kafle <mebinod50@gmail.com>
2022-08-21 08:19:39 +0545

    chore: refactor leave api's

--- itbridge/models/leave.py
+++ itbridge/models/leave.py
@@ -6,17 +6,27 @@ from shared.models import Base
 
 
 class Leave(Base):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Cancelled', 'Cancelled')
+    )
+    TYPES = (
+        ("Festival", "Festival"),
+        ("Sick", "Sick"),
+        ("Marriage", "Marriage"),
+        ("Mourning", "Mourning")
+    )
 
-            users = models.ForeignKey(User, Null=False, on_delete=models.CASCADE, related_name="user_leave")
-            from_date = models.DateTimeField()
-            to_date = models.DateTimeField()
-            stat = (('Pending', 'Pending'), ('Approved', 'Approved'), ('Cancelled', 'Cancelled'))
-            status = models.CharField(max_length=50, choices=stat, default='Pending', null=True)
-            type = models.CharField(max_length=100)
-            reason_text = models.CharField(max_length=100)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name="user_leave")
+    from_date = models.DateTimeField(null=False)
+    to_date = models.DateTimeField(null=False)
+    status = models.CharField(max_length=50, null=False, choices=STATUS, default='Pending')
+    type = models.CharField(max_length=100, null=False, choices=TYPES)
+    reason_text = models.CharField(max_length=100, null=False)
 
-            def __str__(self):
-                return self.email
+    def __str__(self):
+        return self.user.name
 
 
 
